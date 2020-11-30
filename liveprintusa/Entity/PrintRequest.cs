737a4a6593d19eedd2f187358f2107fc8beeaacc
sergiomenesesfarias <sergiomenesesfarias@hotmail.com>
2020-11-29 19:11:17 -0500

    Add project files.

---
+++ liveprintusa/Entity/PrintRequest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace liveprintusa.Entity
+{
+    [DataContract]
+    public class PrintRequest
+    {
+        [DataMember]
+        public int CustomerId { get; set; }
+        [DataMember]
+        public string CustomerKey { get; set; }
+        [DataMember]
+        public string CustomerData { get; set; } // Json format: RefereceNumber, Sender , DestinationName, DestinatioAddress, DestinatioCity, DestinatioState, DestinatioZipcode
+        [DataMember]
+        public byte[] Body { get; set; }
+
+    }
+}
---
+++ liveprintusa/Service.svc.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using liveprintusa.Entity;
+
+namespace liveprintusa
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service.svc or Service.svc.cs at the Solution Explorer and start debugging.
+    public class Service : IService
+    {
+      
+        public int SubmitRequest(PrintRequest lobj)
+        {
+            if (lobj != null)
+            {
+                return 1;
+            }
+            return 0;
+        }
+
+    }
+}
